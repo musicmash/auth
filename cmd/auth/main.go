2370edb13c9ffba79b2c5b77fc2a488cde241dd7
Mikhail Kalinin <m.kalinin@selectel.ru>
2020-07-08 23:35:21 +0300

    Add simple PoC that validates idToken

--- cmd/auth/main.go
+++ cmd/auth/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"golang.org/x/net/context"
+
+	firebase "firebase.google.com/go"
+	"google.golang.org/api/option"
+)
+
+const (
+	serviceAccountFilePath = "serviceAccountKey.json"
+	idToken                = "xxx"
+)
 
 func main() {
-    fmt.Println("auth service started")
+	opt := option.WithCredentialsFile(serviceAccountFilePath)
+	app, err := firebase.NewApp(context.Background(), nil, opt)
+	if err != nil {
+		log.Fatalf("error initializing app: %v\n", err)
+	}
+
+	client, err := app.Auth(context.Background())
+	if err != nil {
+		log.Fatalf("error getting Auth client: %v\n", err)
+	}
+
+	token, err := client.VerifyIDToken(context.Background(), idToken)
+	if err != nil {
+		log.Fatalf("error verifying ID token: %v\n", err)
+	}
+
+	log.Printf("Verified ID token: %v\n", token)
+	log.Printf("user_id: %v", token.UID)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/musicmash/auth
 
 go 1.13
+
+require (
+	firebase.google.com/go v3.13.0+incompatible
+	firebase.google.com/go/v4 v4.0.0 // indirect
+	golang.org/x/net v0.0.0-20190620200207-3b0461eec859
+	google.golang.org/api v0.17.0
+)
