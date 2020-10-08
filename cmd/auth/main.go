d3eb619bab1ada558c1791d8201575d83677dbdd
Mikhail Kalinin <m.kalinin@selectel.ru>
2020-10-08 23:35:01 +0300

    Remove firebase

--- cmd/auth/main.go
+++ cmd/auth/main.go
@@ -1,40 +1,21 @@
 package main
 
 import (
-	"flag"
 	"os"
 	"time"
 
 	"github.com/go-chi/chi"
-	"github.com/musicmash/auth/internal/api/handlers/auth"
 	"github.com/musicmash/auth/internal/api/router"
 	"github.com/musicmash/auth/internal/api/server"
-	"github.com/musicmash/auth/internal/backends/firebase"
 	"github.com/musicmash/auth/internal/log"
 )
 
 func main() {
-	// define cli args
-	serviceAccountFilePath := flag.String(
-		"path-to-service-account",
-		"/etc/auth/serviceAccountKey.json",
-		"abs path to serviceAccountKey.json",
-	)
-	flag.Parse()
-
 	// setup logger
 	log.SetWriters(log.GetConsoleWriter())
 
-	// make backend
-	backend, err := firebase.New(*serviceAccountFilePath)
-	if err != nil {
-		exitIfError(err)
-	}
-
 	// make router
-	authHandler := auth.NewHandler(backend)
 	r := chi.NewRouter()
-	r.Post("/auth", authHandler.DoAuth)
 	router := router.New(r)
 
 	// make http server
